55efcbd2398ff157f1b338fa3aeff7390f049e86
Furkan Evin <furkanevin00@gmail.com>
2025-06-15 14:18:27 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
