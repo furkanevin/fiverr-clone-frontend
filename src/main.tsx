e16f9f3e559139ee8f6a26d16d0aa9b878d76e50
Furkan Evin <furkanevin00@gmail.com>
2025-06-15 12:27:09 +0300

    analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
+import { Analytics } from "@vercel/analytics/next";
 
 const queryClient = new QueryClient();
 
@@ -10,5 +11,6 @@ createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <App />
     <ToastContainer />
+    <Analytics />
   </QueryClientProvider>
 );
